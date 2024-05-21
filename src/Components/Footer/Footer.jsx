c75fe61f3496cc781ad3216f7eb156de36deb050
Ilia Turovchik <umbomaxia@gmail.com>
2024-05-21 17:52:20 +0300

    feat: add footer adaptive

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import classes from "./Footer.module.css"
+import React from 'react';
+import classes from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
     <div className={classes.footer}>
-        <div className={classes.footerContainer}>
-            <div>2024</div>
-            <div>Made by Ilia Turovchik</div>
-            <div>https://github.com/Umbomax</div>
+      <div className={classes.footerContainer}>
+        <div>2024</div>
+        <div>Made by Ilia Turovchik</div>
+        <div>
+          <a href="https://github.com/Umbomax" target="_blank" rel="noopener noreferrer" className={classes.githubLink}>
+            <FontAwesomeIcon icon={faGithub} size="2x" />
+          </a>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
