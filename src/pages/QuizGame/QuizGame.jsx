5c3f266b75a7b6879c5c25a7f8cff0dc1eb4fe51
Ilia <umbomaxia@gmail.com>
2024-08-06 12:53:51 +0300

    feat: infinity swipes

--- src/pages/QuizGame/QuizGame.jsx
+++ src/pages/QuizGame/QuizGame.jsx
@@ -6,23 +6,28 @@ import classes from "./QuizGame.module.css";
 import QuizResultsModal from "../../Components/QuizResultsModal/QuizResultsModal";
 import ImageContainer from "../../Components/ImageContainer/ImageContainer";
 import Timer from "../../Components/Timer/Timer";
+
 import Carousel from "../../Components/Carousel/Carousel";
 
 function QuizGame(props) {
     const location = useLocation();
     const [questions, setQuestions] = useState([]);
-    const [currentSlide, setCurrentSlide] = useState(0);
+    const [currentSlide, setCurrentSlide] = useState(1);
     const [correctAnswersCount, setCorrectAnswersCount] = useState(0);
     const [answersStatus, setAnswersStatus] = useState([]);
     const [answers, setAnswers] = useState([]);
     const [gameEnd, setGameEnd] = useState(false);
     const [modal, setModal] = useState(false);
     const [quizStartTime, setQuizStartTime] = useState(null);
+    const [slides, setSlides] = useState([]);
+    const [transitioning, setTransitioning] = useState(false); // Added for animation
+    const [animationTime, setAnimationTime] = useState(0.6);
     const apiUrl = process.env.REACT_APP_API_URL;
 
     useEffect(() => {
         const fetchedQuestions = location.state?.questions || [];
         setQuestions(fetchedQuestions);
+        setSlides([fetchedQuestions[fetchedQuestions.length - 1], ...fetchedQuestions, fetchedQuestions[0]]);
         const initialAnswersStatus = new Array(fetchedQuestions.length).fill(null);
         setAnswersStatus(initialAnswersStatus);
 
@@ -63,22 +68,64 @@ function QuizGame(props) {
 
 
     const inlineStyle = {
-        width: `${100 * location.state.quizesCount}%`,
+        width: `${100 * (questions.length + 2)}%`, 
     };
 
     const activeSlide = {
         transform: `translateX(${-100 * currentSlide}%)`,
+        transition: transitioning ? `transform ${animationTime}s cubic-bezier(0.5, 0, 0.5, 1)` : 'none',
     };
 
     function nextSlide(e) {
         e.preventDefault();
-        setCurrentSlide((prev) => (prev === questions.length - 1 ? 0 : prev + 1));
+        if (transitioning) return;
+        setTransitioning(true);
+        setCurrentSlide((prev) => prev + 1);
     }
 
     function prevSlide(e) {
         e.preventDefault();
-        setCurrentSlide((prev) => (prev === 0 ? questions.length - 1 : prev - 1));
+        if (transitioning) return;
+        setTransitioning(true);
+        setCurrentSlide((prev) => prev - 1);
     }
+
+    useEffect(() => {
+        if (!transitioning) return;
+        
+        const handleTransitionEnd = () => {
+            if (currentSlide === slides.length - 1) {
+                setCurrentSlide(slides.length - 1); // Установить на последний слайд (невидимый)
+                setTimeout(() => {
+                    setTransitioning(false);
+                    setCurrentSlide(0); // Перейти к первому слайду (невидимый)
+                    setTimeout(() => {
+                        setTransitioning(true);
+                        setCurrentSlide(1); // Перейти к первому слайду (видимый)
+                    }, 200); // Время для завершения перехода с последнего на первый
+                }, 200); // Время для завершения перехода с последнего на первый
+            } else if (currentSlide === 0) {
+                setCurrentSlide(0); // Установить на первый слайд (невидимый)
+                setTimeout(() => {
+                    setTransitioning(false);
+                    setCurrentSlide(slides.length - 1); // Перейти к последнему слайду (невидимый)
+                    setTimeout(() => {
+                        setTransitioning(true);
+                        setCurrentSlide(slides.length - 2); // Перейти к последнему слайду (видимый)
+                    }, 200); // Время для завершения перехода с первого на последний
+                }, 200); // Время для завершения перехода с первого на последний
+            } else {
+                setTransitioning(false);
+            }
+        };
+    
+        const transitionTime = (currentSlide === slides.length - 1 || currentSlide === 0) ? 100 : 600;
+        const timer = setTimeout(handleTransitionEnd, transitionTime);
+    
+        return () => clearTimeout(timer);
+    }, [currentSlide, transitioning, slides.length]);
+    
+
 
     function checkAnswer(answer, questionIdx, answerIdx) {
         if (gameEnd || answersStatus[questionIdx] !== null) {
@@ -110,7 +157,6 @@ function QuizGame(props) {
         setCorrectAnswersCount(correctCount);
         setTimeout(() => setModal(true), 1000);
 
-        // Отправка данных на сервер по окончании викторины
         const token = localStorage.getItem('token');
         const quizId = location.state._id;
         const quizEndTime = Date.now();
@@ -147,67 +193,63 @@ function QuizGame(props) {
 
             <div className={classes.quizItemsWrapper}>
                 <div style={inlineStyle} className={classes.quizItemsContainer}>
-                    {questions.map((question, idx) => (
-                        <div style={activeSlide} key={idx} className={classes.quizItem}>
-                            <h1 className={classes.questionText}>{question.question}</h1>
-                            <div className={classes.answersContainer}>
-                                <div className={classes.questionForQuiz}>
-                                <ImageContainer  src={question.questionImage} altName={"question"}></ImageContainer>
-                                </div>
-                               
-
-                                <div className={`${classes.answersWrapper} ${location.state.type === "1q4textanswer" || location.state.type === "1q1textanswer" ? classes.notGrid:""}`}>
-                                {location.state.type === "1q4textanswer" || location.state.type === "1q1textanswer" ? (
-                                    <div className={classes.textAnswers}>
-                                        {answers[idx].map((answer, answerIdx) => (
-                                            <div
-                                                key={answerIdx}
-                                                className={`${classes.answer} ${
-                                                    answersStatus[idx]?.isCorrect === false && answerIdx === answersStatus[idx].correctAnswerIdx
-                                                        ? classes.correct
-                                                        : answersStatus[idx]?.isCorrect === false && answerIdx === answersStatus[idx].selectedAnswerIdx
-                                                        ? classes.incorrect
-                                                        : answersStatus[idx]?.isCorrect === true && answerIdx === answersStatus[idx].selectedAnswerIdx
-                                                        ? classes.correct
-                                                        : ""
-                                                }`}
-                                                onClick={() => checkAnswer(answer, idx, answerIdx)}
-                                            >
-                                                {answer}
-                                            </div>
-                                        ))}
-                                    </div>
-                                ) : (
-                                    answers[idx].map((answer, answerIdx) => (
-                                        <div
-                                            key={answerIdx}
-                                            className={`${classes.answer} ${
-                                                answersStatus[idx]?.isCorrect === false && answerIdx === answersStatus[idx].correctAnswerIdx
-                                                    ? classes.correct
-                                                    : answersStatus[idx]?.isCorrect === false && answerIdx === answersStatus[idx].selectedAnswerIdx
-                                                    ? classes.incorrect
-                                                    : answersStatus[idx]?.isCorrect === true && answerIdx === answersStatus[idx].selectedAnswerIdx
-                                                    ? classes.correct
-                                                    : ""
-                                            }`}
-                                            onClick={() => checkAnswer(answer, idx, answerIdx)}
-                                        >
-                                            <div className={classes.questionImageWrapper}>
-                                                <ImageContainer src={answer} altName={"answer"}></ImageContainer>
-                                            </div>
-                                            
-                                         
+                    {slides.map((question, idx) => (
+                        <div style={activeSlide} key={idx} className={`${classes.quizItem} ${classes.carousel_item}`}>
+                            {idx > 0 && idx < slides.length - 1 && (
+                                <>
+                                    <h1 className={classes.questionText}>{question.question}</h1>
+                                    <div className={classes.answersContainer}>
+                                        <div className={classes.questionForQuiz}>
+                                            <ImageContainer src={question.questionImage} altName={"question"}></ImageContainer>
                                         </div>
-                                    ))
-                                )}
-                            </div>
 
-                            </div>
+                                        <div className={`${classes.answersWrapper} ${location.state.type === "1q4textanswer" || location.state.type === "1q1textanswer" ? classes.notGrid:""}`}>
+                                            {location.state.type === "1q4textanswer" || location.state.type === "1q1textanswer" ? (
+                                                <div className={classes.textAnswers}>
+                                                    {answers[idx - 1].map((answer, answerIdx) => (
+                                                        <div
+                                                            key={answerIdx}
+                                                            onClick={() => checkAnswer(answer, idx - 1, answerIdx)}
+                                                            className={`${classes.textAnswer} ${
+                                                                answersStatus[idx - 1]?.selectedAnswerIdx === answerIdx
+                                                                    ? answersStatus[idx - 1]?.isCorrect
+                                                                        ? classes.correctAnswer
+                                                                        : classes.incorrectAnswer
+                                                                    : ""
+                                                            }`}
+                                                        >
+                                                            {answer}
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            ) : (
+                                                <div className={classes.answersGrid}>
+                                                    {answers[idx - 1].map((answer, answerIdx) => (
+                                                        <div
+                                                            key={answerIdx}
+                                                            onClick={() => checkAnswer(answer, idx - 1, answerIdx)}
+                                                            className={`${classes.answer} ${
+                                                                answersStatus[idx - 1]?.selectedAnswerIdx === answerIdx
+                                                                    ? answersStatus[idx - 1]?.isCorrect
+                                                                        ? classes.correctAnswer
+                                                                        : classes.incorrectAnswer
+                                                                    : ""
+                                                            }`}
+                                                        >
+                                                            <ImageContainer src={answer} altName={"answer"}></ImageContainer>
+                                                        </div>
+                                                    ))}
+                                                </div>
+                                            )}
+                                        </div>
+                                    </div>
+                                </>
+                            )}
                         </div>
                     ))}
                 </div>
             </div>
-
+            
             <div className={classes.controlsContainer}>
                 <button onClick={prevSlide}>Previous</button>
                 {location.state.isTimed && !gameEnd && <Timer quizTime={location.state.quizTime} onTimeEnd={handleTimeEnd} />}
